e162cd966f6925d78c00abd818aeab63ba02497a
bd2051 <bd2051@mail.ru>
2018-04-03 18:30:58 +0300

    исправлено замечание по Д20

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-    var btn = document.querySelector(".js-motel-search-button");
     var popup = document.querySelector(".modal-motel-search");
+    var btn = document.querySelector(".js-motel-search-button");
+
+    var form = popup.querySelector(".search-form");
     var arival = popup.querySelector("[name=search-arival-date]");
     var departure = popup.querySelector("[name=search-departure-date]");
-    var form = popup.querySelector(".search-form");
     var persons = popup.querySelector("[name=search-persons]");
     var child = popup.querySelector("[name=search-child]");
     
     var isStorageSupport = true;
     var storagepersons = "";
     var storagechild = "";
-  
+    
+    var hiddenmap = document.querySelector(".sedona-map img");
+    var modalmap = document.querySelector(".sedona-map iframe")
+    
     try {
       storagepersons = localStorage.getItem("persons");
       storagechild = localStorage.getItem("child", child.value);
       } catch (err) {
       isStorageSupport = false;
     }
+    popup.classList.add("js-modal-hide")
+    hiddenmap.classList.add("js-modal-hide")
+    modalmap.classList.remove("js-modal-hide")
 
     btn.addEventListener("click", function (evt) {
       evt.preventDefault();
