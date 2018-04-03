91c57cef1e8c5806fc42d1374d240c6f508e827f
bd2051 <bd2051@mail.ru>
2018-04-03 18:16:41 +0300

    исправлено замечание д17

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
     var btn = document.querySelector(".js-motel-search-button");
     var popup = document.querySelector(".modal-motel-search");
     var arival = popup.querySelector("[name=search-arival-date]");
+    var departure = popup.querySelector("[name=search-departure-date]");
     var form = popup.querySelector(".search-form");
     var persons = popup.querySelector("[name=search-persons]");
     var child = popup.querySelector("[name=search-child]");
@@ -19,6 +20,7 @@
     btn.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.toggle("js-modal-show");
+      popup.classList.remove("js-modal-error");
       arival.focus();
       if (storagepersons) {
         persons.value = storagepersons;
@@ -29,12 +31,18 @@
     });
     
     form.addEventListener("submit", function (evt) {
-      <!-- evt.preventDefault(); -->
-      if (isStorageSupport) {
-        localStorage.setItem("persons", persons.value);
-        localStorage.setItem("child", child.value);
+      if (!persons.value || !child.value || !arival.value || !departure.value) {
+        evt.preventDefault();
+        console.log("Нужно ввести логин и пароль");
+      popup.classList.remove("js-modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("js-modal-error");
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem("persons", persons.value);
+          localStorage.setItem("child", child.value);
+        }
       }
-      console.log("сохранено в localStorage");
     });
     
     window.addEventListener("keydown", function (evt) {
@@ -42,6 +50,7 @@
       evt.preventDefault();
       if (popup.classList.contains("js-modal-show")) {
         popup.classList.remove("js-modal-show");
+        popup.classList.remove("js-modal-error");
       }
     }
   });
